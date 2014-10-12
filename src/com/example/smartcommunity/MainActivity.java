389b3d3ee5aa179e391eb5eff4dd8327774e084c
kaffa <my5156@sina.cn>
2014-10-12 11:36:29 +0800

    stage

--- src/com/example/smartcommunity/MainActivity.java
+++ src/com/example/smartcommunity/MainActivity.java
@@ -32,10 +32,10 @@ public class MainActivity extends Activity {
 										"自助银行", "家庭理财", "携程票务", "费用充值",
 										"网上购物", "人寿保险", "亲子资讯", "家装服务"
 										};
-	private static String[] images = {"neighbourhood", "pic", "eleme", "icon_payment", 
-										"meituan", "tongquwang", "kuaidi100", "pic", 
+	private static String[] images = {"icon_neighbourhood", "pic", "icon_eleme", "icon_payment", 
+										"icon_meituan", "icon_tongquwang", "icon_kuaidi100", "pic", 
 										"pic", "pic", "pic", "pic",
-										"pic", "pic", "dididache", "damai",
+										"pic", "pic", "icon_dididache", "icon_damai",
 										"pic", "pic", "pic", "pic",
 										"pic", "pic", "pic", "pic"
 										};
@@ -64,7 +64,7 @@ public class MainActivity extends Activity {
 				int image = (Integer)item.get("item_image");
 				//Toast.makeText(MainActivity.this, "未安装此程序", Toast.LENGTH_LONG).show();
 				switch (image) {
-				case R.drawable.eleme:
+				case R.drawable.icon_eleme:
 					Intent eleme=new Intent();    
 					eleme.setClassName("me.ele", "me.ele.activity.SplashActivity");  
 					try {
@@ -75,7 +75,7 @@ public class MainActivity extends Activity {
 					}
 					break;
 					
-				case R.drawable.meituan:
+				case R.drawable.icon_meituan:
 					Intent meituan=new Intent();    
 					meituan.setClassName("com.sankuai.meituan", "com.sankuai.meituan.activity.Welcome");
 					try {
@@ -86,7 +86,7 @@ public class MainActivity extends Activity {
 					}
 					break;
 					
-				case R.drawable.tongquwang:
+				case R.drawable.icon_tongquwang:
 					Intent tongquwang=new Intent();    
 					tongquwang.setClassName("com.tongqu", "com.tongqu.welcome.WelcomeActivity"); 
 					try {
@@ -97,7 +97,7 @@ public class MainActivity extends Activity {
 					}
 					break;
 					
-				case R.drawable.kuaidi100:
+				case R.drawable.icon_kuaidi100:
 					Intent intent=new Intent();    
 					intent.setClassName("com.Kingdee.Express", "com.Express.Activity.Splash");
 					try {
@@ -108,7 +108,7 @@ public class MainActivity extends Activity {
 					}
 					break;
 					
-				case R.drawable.dididache:
+				case R.drawable.icon_dididache:
 					Intent dididache=new Intent();    
 					dididache.setClassName("com.sdu.didi.psnger", "com.didi.frame.MainActivity");  
 					try {
@@ -119,7 +119,7 @@ public class MainActivity extends Activity {
 					}
 					break;
 					
-				case R.drawable.damai:
+				case R.drawable.icon_damai:
 					Intent damai=new Intent();    
 					damai.setClassName("cn.damai", "cn.damai.activity.MainSplashActivity");  
 					try {
@@ -130,7 +130,7 @@ public class MainActivity extends Activity {
 					}
 					break;
 					
-				case R.drawable.neighbourhood:
+				case R.drawable.icon_neighbourhood:
 					Intent neighCenterIntent = new Intent(MainActivity.this, NeighCenter.class);
 					startActivity(neighCenterIntent);
 					break;
